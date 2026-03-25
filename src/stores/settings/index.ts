ff183dd1dbbb67126f2e95d9d0373fd8dfd11684
milisp <milisp@proton.me>
2026-03-25 07:35:01 -0400

    feat: remove PromptOptimizerStore and its export from settings.

--- src/stores/settings/index.ts
+++ src/stores/settings/index.ts
@@ -1,5 +1,4 @@
 export { useLayoutStore } from "./layoutStore";
-export { usePromptOptimizerStore } from "./PromptOptimizerStore";
 export { useSettingsStore } from "./SettingsStore";
 export { useThemeStore } from "./ThemeStore";
 
